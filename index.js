a1b1156207aa5ac0f50c3d0249bf9d2dbc70f2f8
unknown <@keykloud.com>
2022-01-28 17:55:17 +0530

    Scoket mode

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*
 Created by Rahul Yadav on 28-01-2022
 */
+/*
 const { App } = require('@slack/bolt');
 const { response } = require('express');
 require('dotenv').config();
@@ -74,3 +75,73 @@ app.shortcut('who_am_i', async({
   
     console.log('⚡️ Bolt app is running!');
   })();
+   */
+  const { App, SocketModeReceiver } = require('@slack/bolt');
+ require('dotenv').config();
+const socketModeReceiver = new SocketModeReceiver({
+  appToken: process.env.SLACK_APP_TOKEN,
+
+  // enable the following if you want to use OAuth
+  // clientId: process.env.CLIENT_ID,
+  // clientSecret: process.env.CLIENT_SECRET,
+  // stateSecret: 'my-state-secret',
+  // scopes: ['channels:read', 'chat:write', 'app_mentions:read', 'channels:manage', 'commands'],
+});
+
+const app = new App({
+  receiver: socketModeReceiver,
+  // disable token line below if using OAuth
+  token: process.env.SLACK_BOT_TOKEN
+});
+
+app.message('rahul', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say(`Hey there <@${message.user}>!`);
+  });
+  
+
+
+
+// Shortcuts
+app.shortcut('who_am_i', async({
+    shortcut,
+    ack,
+    client
+  }) => {
+    try {
+      //acknowledge shortcut req
+      await ack();
+      //call the view.open method
+      const result = await client.views.open({
+        trigger_id : shortcut.trigger_id,
+        view: {
+          type : "modal",
+          title: {
+            type: "plain_text",
+            text: "My App"
+          },
+          close: {
+            type:"plain_text",
+            text:"Close"
+          },
+          blocks: [{
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "hello KeyKloud Team"
+            }
+          }]
+        }
+      });
+      console.log(result);
+  
+    } catch(error){
+      console.error(error);
+    }
+  
+  });
+
+(async () => {
+  await app.start();
+  console.log('⚡️ Bolt app started');
+})();
